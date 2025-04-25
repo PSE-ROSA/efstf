6d230ad6c0bfb8304e1fbc07191e9c4e108626ec
huangleikn <leih@cn.ibm.com>
2025-04-25 13:18:56 +0800

    update sg rule

--- main.tf
+++ main.tf
@@ -52,6 +52,29 @@ resource "aws_efs_backup_policy" "policy" {
 #   }
 # }
 
+# 更新现有安全组规则，允许 NFS 流量
+resource "aws_security_group_rule" "allow_nfs_inbound" {
+  security_group_id = var.existing_security_group_id
+
+  type              = "ingress"
+  from_port         = 2049
+  to_port           = 2049
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] # 允许所有 IP 访问 NFS，按需调整
+  description       = "Allow NFS inbound traffic for EFS"
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  security_group_id = var.existing_security_group_id
+
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1" # 允许所有协议
+  cidr_blocks       = ["0.0.0.0/0"] # 允许所有 IP 访问
+  description       = "Allow all outbound traffic"
+}
+
 # 创建挂载目标
 resource "aws_efs_mount_target" "example" {
   count         = length(var.subnet_ids)
